0c82690991426a49b030abf534271d85b9c80922
Sergey Ishunkin <128365862+ishunkinsa@users.noreply.github.com>
2023-05-21 12:00:47 +0300

    Update E.py

--- E.py
+++ E.py
@@ -1,14 +1,15 @@
 """Даны две строки, состоящие из строчных латинских букв. 
 Требуется определить, являются ли эти строки анаграммами, т. е.
 отличаются ли они только порядком следования символов."""
+import sys 
 
 def Anogram_check(s1,s2):
     if sorted(s1) == sorted(s2):
         return True
     return False
 
-s1 = input()
-s2 = input()
+s1 = sys.stdin.readline().strip()
+s2 = sys.stdin.readline().strip()
 if Anogram_check(s1,s2):
     print(1)
 else:
