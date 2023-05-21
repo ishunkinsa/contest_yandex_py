777f1e5c2a5010913193e4bc662609f0b57d867d
Sergey Ishunkin <128365862+ishunkinsa@users.noreply.github.com>
2023-05-21 11:58:52 +0300

    Update E.py

--- E.py
+++ E.py
@@ -2,3 +2,14 @@
 Требуется определить, являются ли эти строки анаграммами, т. е.
 отличаются ли они только порядком следования символов."""
 
+def Anogram_check(s1,s2):
+    if sorted(s1) == sorted(s2):
+        return True
+    return False
+
+s1 = input()
+s2 = input()
+if Anogram_check(s1,s2):
+    print(1)
+else:
+    print (0)
