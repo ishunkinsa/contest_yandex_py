51a0696a2390060d9947d642a968252f19335b94
Sergey Ishunkin <128365862+ishunkinsa@users.noreply.github.com>
2023-05-21 18:12:11 +0300

    Create E_mark_2.py

---
+++ E_mark_2.py
@@ -0,0 +1,19 @@
+import sys
+from collections import defaultdict
+
+def dictFromString(s):
+    d =defaultdict(int)
+    for c in s:
+        d[c] += 1
+    return d
+def Anogram_check(a, b):
+    return dictFromString(a) == dictFromString(b)
+
+
+s1 = sys.stdin.readline().strip()
+s2 = sys.stdin.readline().strip()
+
+if Anogram_check(s1,s2):
+    print(1)
+else:
+    print (0)
